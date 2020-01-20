911f5b6a49eeae2867792c03afcc7db24260e7be
Gerry <gerrygrolt@gmail.com>
2020-01-20 09:14:30 +0800

    Put markdown for Hernquist and NFW in correct location

--- src/types/dark_matter_types.jl
+++ src/types/dark_matter_types.jl
@@ -1,35 +1,35 @@
 abstract type Dark_matter end
 
+"""
+    Hernquist(;dm_mass = 186.9,
+                dm_a = 34.5)
+
+    Creates a Hernquist analytic dark matter mass potential.
+    Default values are shown above.
+"""
 struct Hernquist <: Dark_matter
     dm_mass::Float64
     dm_a::Float64
 
-    """
-        Hernquist(;dm_mass = 186.9,
-                    dm_a = 34.5)
-
-        Creates a Hernquist analytic dark matter mass potential.
-        Default values are shown above.
-    """
     function Hernquist(dm_mass::Float64 = 184.9,
                         dm_a::Float64 = 34.5)
         new(dm_vm, dm_a)
     end
 end
 
+"""
+    NFW(;dm_vm = 186.9,
+            dm_a = 34.5,
+            dm_rhof = 0.035)
+
+    Creates an NFW analytic dark matter mass potential.
+    Default values are shown above.
+"""
 struct NFW <: Dark_matter
     dm_vm::Float64
     dm_a::Float64
     dm_rhof::Float64
 
-    """
-        NFW(;dm_vm = 186.9,
-                dm_a = 34.5,
-                dm_rhof = 0.035)
-
-        Creates an NFW analytic dark matter mass potential.
-        Default values are shown above.
-    """
     function NFW(dm_vm::Float64 = 186.9,
                     dm_a::Float64 = 34.5,
                     dm_rhof::Float64 = 0.035)
