f77c154c1a385fa9b5953655f33dec0da033071f
Gerry <gerrygrolt@gmail.com>
2020-02-18 10:24:13 +0800

    Add Environment constructor allowing arrays to be passed to create multiple environemtn in one go

--- src/types/environment_type.jl
+++ src/types/environment_type.jl
@@ -5,7 +5,8 @@
 """
     Environment(z, inc_deg, r200, mass2light, blur)
 
-Creates a `struct` containing environmental parameters required for a mock observation of a simulated galaxy.
+Creates a `struct` containing environmental parameters required for a mock observation of a simulated galaxy. Each argument can either be an array
+or a single value. If an array is passed then a corresponding array of Environments will be returned containing every permutation of the given parameters.
 
 Parameters:\n
     z               The projected redshift at which the observation is made.
@@ -31,31 +32,90 @@ struct Environment
         new(z, inc_deg, r200, mass2light, blur)
     end
 
-    function Environment(z::Float64,
-                        inc_deg::Real,
-                        r200::Real,
-                        blur::Blur)
+    function Environment(z_array::Union{Float64, Array{Float64, 1}, AbstractRange{Float64}},
+                        inc_deg_array::Union{Real, Array{Real, 1}, AbstractRange{Real}},
+                        r200_array::Union{Real, Array{Real, 1}, AbstractRange{Real}},
+                        mass2light_array::Union{Real, Array{Real, 1}, AbstractRange{Real}},
+                        blur_array::Union{Blur, Array{Blur, 1}, AbstractRange{Real}})
+        envir_array = Environment[]
 
-        mass2light=1.
-        new(z, inc_deg, r200, mass2light, blur)
+        for z in z_array
+            for inc_deg in inc_deg_array
+                for r200 in r200_array
+                    for mass2light in mass2light_array
+                        for blur in blur_array
+                            envir = new(z, inc_deg, r200, mass2light, blur)
+                            push!(envir_array, envir)
+                        end
+                    end
+                end
+            end
+        end
+
+        return envir_array
     end
 
-    function Environment(z::Float64,
-                        inc_deg::Real,
-                        r200::Real,
-                        mass2light::Real)
+    function Environment(z_array::Union{Float64, Array{Float64, 1}, AbstractRange{Float64}},
+                        inc_deg_array::Union{Real, Array{Real, 1}, AbstractRange{<:Real}},
+                        r200_array::Union{Real, Array{Real, 1}, AbstractRange{<:Real}},
+                        blur_array::Union{Blur, Array{Blur, 1}})
+        envir_array = Environment[]
+
+        mass2light = 1.
+
+        for z in z_array
+            for inc_deg in inc_deg_array
+                for r200 in r200_array
+                    for blur in blur_array
+                        envir = new(z, inc_deg, r200, mass2light, blur)
+                        push!(envir_array, envir)
+                    end
+                end
+            end
+        end
+
+        return envir_array
+    end
+
+    function Environment(z_array::Union{Float64, Array{Float64, 1}, AbstractRange{Float64}},
+                        inc_deg_array::Union{Real, Array{Real, 1}, AbstractRange{<:Real}},
+                        r200_array::Union{Real, Array{Real, 1}, AbstractRange{<:Real}},
+                        mass2light_array::Union{Real, Array{Real, 1}, AbstractRange{<:Real}})
+        envir_array = Environment[]
 
         blur = nothing
-        new(z, inc_deg, r200, mass2light, blur)
+
+        for z in z_array
+            for inc_deg in inc_deg_array
+                for r200 in r200_array
+                    for mass2light in mass2light_array
+                        envir = new(z, inc_deg, r200, mass2light, blur)
+                        push!(envir_array, envir)
+                    end
+                end
+            end
+        end
+
+        return envir_array
     end
 
-    function Environment(z::Float64,
-                        inc_deg::Real,
-                        r200::Real)
-        mass2light=1.
+    function Environment(z_array::Union{Float64, Array{Float64, 1}, AbstractRange{Float64}},
+                        inc_deg_array::Union{Real, Array{Real, 1}, AbstractRange{<:Real}},
+                        r200_array::Union{Real, Array{Real, 1}, AbstractRange{<:Real}})
+        envir_array = Environment[]
+
+        mass2light = 1.
         blur = nothing
-        new(z, inc_deg, r200, mass2light, blur)
-    end
 
+        for z in z_array
+            for inc_deg in inc_deg_array
+                for r200 in r200_array
+                    envir = new(z, inc_deg, r200, mass2light, blur)
+                    push!(envir_array, envir)
+                end
+            end
+        end
+
+        return envir_array
     end
 end
