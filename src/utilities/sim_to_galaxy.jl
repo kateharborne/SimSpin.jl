139dc2402de64194946e5752f7fc7c89eb00e2cb
kateharborne <katherine.harborne@icrar.org>
2020-06-17 22:48:04 +0800

    Update to sim_to_galaxy() to allow user specified centre positions.

--- src/utilities/sim_to_galaxy.jl
+++ src/utilities/sim_to_galaxy.jl
@@ -3,16 +3,24 @@
 # Original author: Katherine Harborne
 
 """
-    sim_to_galaxy(sim_data)
+    sim_to_galaxy(sim_data; centre)
 
 Returns particle data wrt centre of galaxy in both spatial and velocity space instead of wrt arbitrary point in simulation.
+Centre in position or centre in both position and velocity can be optionally specified. Else, the median of each distribution will be used as centre.
 """
-function sim_to_galaxy(sim_data::Array{Sim_particle, 1})
+function sim_to_galaxy(sim_data::Array{Sim_particle, 1}; centre::Array{Float64, 1} = Float64[])
 
     galaxy_data = Array{Galaxy_particle, 1}(undef, length(sim_data))
 
     lum_data = sim_data[findall(x -> typeof(x) != Sim_dark, sim_data)]   #get all luminous particles
-    centre = galaxy_centre(lum_data)                    #use luminous particles only to find centre
+    if isempty(centre)
+        centre = galaxy_centre(lum_data)                    #use luminous particles only to find centre
+    elseif length(centre) == 3
+        vel_centre = galaxy_centre(lum_data)[4:6]
+        centre = [centre; vel_centre]
+    elseif length(centre) != 6
+        error("Unable to process centre. Expected format [xcen, ycen, zcen] OR [xcen, ycen, zcen, vxcen, vycen, vzcen].")
+    end
     rot_mat = galaxy_orient(lum_data, centre)           #use luminous particles only to find rotation matrix
 
     Threads.@threads for index in eachindex(sim_data)
