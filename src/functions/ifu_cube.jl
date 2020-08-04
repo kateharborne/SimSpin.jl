ab04e10542d91767ce97186de1490c0b6f5b4b0e
Gerry <gerrygralton@gmail.com>
2020-08-04 12:04:45 +0800

    Efficiency: Use CartesianIndices for indexing in ifu_cube()

--- src/functions/ifu_cube.jl
+++ src/functions/ifu_cube.jl
@@ -23,31 +23,21 @@ function ifu_cube(flux_grid::Array{Float64, 3},
                     observe::Observation)
 
     cube = zeros(Float64, (observe.sbin, observe.sbin, observe.vbin))
+    part_grid = reshape(parts_in_cell, (observe.sbin, observe.sbin, observe.vbin))
 
-    sbin2 = observe.sbin^2
+    Threads.@threads for index in CartesianIndices(part_grid) # for each cell in cube
 
-    Threads.@threads for index in 1:length(parts_in_cell) # for each cell in cube
+        cell = part_grid[index]
 
-        cell = parts_in_cell[index]
-
-        coord = [index % observe.sbin, index % (sbin2) ÷ observe.sbin + 1, index ÷ (sbin2) + 1]
-        if(index % observe.sbin == 0 && index % (sbin2) == 0 )
-            coord = [observe.sbin, observe.sbin, index ÷ (sbin2)]
-        elseif(index % observe.sbin == 0 && index % (sbin2) != 0 )
-            coord = [observe.sbin, index % (sbin2) ÷ observe.sbin, index ÷ (sbin2) + 1]
-        end
-
-        num_of_parts = length(cell) # number of particles in that cell
-
-        if num_of_parts > 0  # if particles in that cell
+        if length(cell) > 0  # if particles in that cell
             cell_mass = sum(getfield.(cell, :mass))
 
             for particle in cell
-                cell_flux = flux_grid[coord[1], coord[2], coord[3]] * particle.mass / cell_mass
+                cell_flux = flux_grid[index] * particle.mass / cell_mass
 
                 distribution = Normal(particle.obs.vy_obs, observe.lsf_size)  #Normal distribution of particle's vy_obs
                 # adding the "gaussians" of each particle to the velocity bins
-                cube[coord[1], coord[2], :] .+= diff(cell_flux .* cdf.(distribution, observe.vseq))
+                cube[index[1], index[2], :] .+= diff(cell_flux .* cdf.(distribution, observe.vseq))
             end
         end
     end
